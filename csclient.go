47e4e67418d7e20cd669f71d00beeeea6004005d
DHacky <dhgacky@gmail.com>
2019-01-29 21:26:14 +0800

    new CSClient

--- csclient.go
+++ csclient.go
@@ -1,7 +1,10 @@
 package mfscli
 
 import (
+	"fmt"
+	"github.com/golang/glog"
 	"net"
+	"time"
 )
 
 // chunk server client
@@ -26,4 +29,26 @@ type CSData struct {
 	ChunkId    uint64
 	Version    uint64
 	CSItems    CSItemMap
+}
+
+func NewCSClient(t *CSItem) (c *CSClient, err error) {
+	c = new(CSClient)
+	addr := fmt.Sprintf("%d.%d.%d.%d:%d", t.Ip>>24, 0xff&(t.Ip>>16),
+		0xff&(t.Ip>>8), 0xff&t.Ip, t.Port)
+	var conn net.Conn
+	for i := 0; i < 3; i++ {
+		conn, err = net.DialTimeout("tcp", addr, time.Minute)
+		if err == nil {
+			c.conn = conn
+			break
+		}
+		glog.V(8).Infof("connect chunk master error: %v retry #%d", err, i+1)
+		time.Sleep(time.Duration(i+1) * time.Second)
+	}
+	if err != nil {
+		return
+	}
+	c.Version = GetVersion(t.Version)
+	glog.V(8).Infof("connect chunk master %s successfully", addr)
+	return
 }
